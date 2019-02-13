ab5bd1103d6f4d0df92b17e2d64ab774dd605615
Arman Feili <armanfc.enter@yahoo.com>
2019-02-13 12:52:40 +0330

    fixed untracked files

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -7,3 +7,6 @@ module.exports = {
 // the process.env.MONGO_URI, and the process.env.SECRET_OR_KEY, are somthing
 // that just server can findout what's they are.
 // so they're suitable for pushing to servers like heroku or so on
+
+// MONGODB_URI
+// mongodb://arman756:arman756@devConnectorCluster-shard-00-00-6xumt.mongodb.net:27017,devConnectorCluster-shard-00-01-6xumt.mongodb.net:27017,devConnectorCluster-shard-00-02-6xumt.mongodb.net:27017/test?ssl=true&replicaSet=devConnectorCluster-shard-0&authSource=admin&retryWrites=true
